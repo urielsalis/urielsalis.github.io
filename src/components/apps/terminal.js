780c73e1f566c4f56a3bd93778dde0eb05d245d8
Uriel Salischiker <uriel@urielsalis.me>
2021-05-01 20:44:54 +0200

    Update terminal.js

--- src/components/apps/terminal.js
+++ src/components/apps/terminal.js
@@ -12,13 +12,11 @@ export class Terminal extends Component {
         this.prev_commands = [];
         this.commands_index = -1;
         this.child_directories = {
-            root: ["books", "projects", "personal-documents", "skills", "languages", "PDPU", "interests"],
-            PDPU: ["Sem-6"],
-            books: ["Eric-Jorgenson_The-Almanack-of-Naval-Ravikant.pdf", "Elon Musk: How the Billionaire CEO of SpaceX.pdf", "The $100 Startup_CHRIS_GUILLEBEAU.pdf", "The_Magic_of_Thinking_Big.pdf"],
-            skills: ["Front-end development", "React.js", "jQuery", "Flutter", "Express.js", "SQL", "Firebase"],
-            projects: ["vivek9patel-personal-portfolio", "synonyms-list-react", "economist.com-unlocked", "Improve-Codeforces", "flutter-banking-app", "Meditech-Healthcare", "CPU-Scheduling-APP-React-Native"],
-            interests: ["Software Engineering", "Deep Learning", "Computer Vision"],
-            languages: ["Javascript", "C++", "Java", "Dart"],
+            root: ["projects", "personal-documents", "skills", "languages", "interests"],
+            skills: ["Back-end development", "Kotlin", "Java"],
+            projects: ["urielsalis-personal-portfolio", "arisakt", "r-isa", "minecrafthopper"],
+            interests: ["Software Engineering", "Music"],
+            languages: ["Java", "Kotlin"],
         };
         this.state = {
             terminal: [],
@@ -56,7 +54,7 @@ export class Terminal extends Component {
             <React.Fragment key={id}>
                 <div className="flex w-full h-5">
                     <div className="flex">
-                        <div className=" text-ubt-green">vivek@Dell</div>
+                        <div className=" text-ubt-green">urielsalis@Urielsalis</div>
                         <div className="text-white mx-px font-medium">:</div>
                         <div className=" text-ubt-blue">{this.current_directory}</div>
                         <div className="text-white mx-px font-medium mr-1">$</div>
